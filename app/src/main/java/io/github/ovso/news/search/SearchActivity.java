3fda67ec3d01d0b6008b02ba6930fb21df95a3bb
Oh jaeho <ovsoce@gmail.com>
2018-04-30 16:53:18 +0900

    Insert SearchView

--- app/src/main/java/io/github/ovso/news/search/SearchActivity.java
+++ app/src/main/java/io/github/ovso/news/search/SearchActivity.java
@@ -1,9 +1,12 @@
 package io.github.ovso.news.search;
 
+import android.support.v7.widget.SearchView;
+import butterknife.BindView;
 import io.github.ovso.news.R;
 import io.github.ovso.news.framework.BaseActivity;
 
 public class SearchActivity extends BaseActivity implements SearchViewPresenter.View {
+  @BindView(R.id.searchview) SearchView searchView;
 
   @Override protected int getLayoutResId() {
     return R.layout.activity_search;
