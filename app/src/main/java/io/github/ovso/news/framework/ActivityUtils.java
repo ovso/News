dde444c9f9a297316c410346a1a49b0759129474
jaeho <ovsoce@mycelebs.com>
2018-05-21 10:57:09 +0900

    Attatched DraggableSwipeableExample..

--- app/src/main/java/io/github/ovso/news/framework/ActivityUtils.java
+++ app/src/main/java/io/github/ovso/news/framework/ActivityUtils.java
@@ -3,13 +3,14 @@ package io.github.ovso.news.framework;
 import android.app.Activity;
 import android.content.Intent;
 import io.github.ovso.news.listup.ListUpActivity;
+import io.github.ovso.news.listup2.DraggableSwipeableExampleActivity;
 import io.github.ovso.news.web.WebActivity;
 import io.github.ovso.news.search.SearchActivity;
 
 public class ActivityUtils {
 
   public static void startActivityListUp(Activity activity) {
-    Intent intent = new Intent(activity, ListUpActivity.class);
+    Intent intent = new Intent(activity, DraggableSwipeableExampleActivity.class);
     activity.startActivity(intent);
     activity.finish();
   }
