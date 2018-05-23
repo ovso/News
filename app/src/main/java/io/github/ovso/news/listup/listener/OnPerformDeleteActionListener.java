4d6c1826e67ef84d67fabca14ec58bf486a47bd7
jaeho <ovsoce@mycelebs.com>
2018-05-23 09:22:02 +0900

    Remove OnPerformDeleteActionListener

--- app/src/main/java/io/github/ovso/news/listup/listener/OnPerformDeleteActionListener.java
+++
@@ -1,7 +0,0 @@
-package io.github.ovso.news.listup.listener;
-
-import io.github.ovso.news.db.WebsiteEntity;
-
-public interface OnPerformDeleteActionListener {
-  void onSwipeDelete(WebsiteEntity remove);
-}
