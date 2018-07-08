6a0f6ff14887cfd2daa1e50f7db50d6f9a37d99b
jaeho <ovsoce@gmail.com>
2018-07-09 01:17:19 +0900

    Add sheet fab

---
+++ app/src/main/java/io/github/ovso/news/listup/SheetFab.java
@@ -0,0 +1,56 @@
+package io.github.ovso.news.listup;
+
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import com.gordonwong.materialsheetfab.AnimatedFab;
+
+public class SheetFab extends FloatingActionButton implements AnimatedFab {
+
+  public SheetFab(Context context) {
+    super(context);
+  }
+
+  public SheetFab(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public SheetFab(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  /**
+   * Shows the FAB.
+   */
+  @Override
+  public void show() {
+    show(0, 0);
+  }
+
+  /**
+   * Shows the FAB and sets the FAB's translation.
+   *
+   * @param translationX translation X value
+   * @param translationY translation Y value
+   */
+  @Override
+  public void show(float translationX, float translationY) {
+    // NOTE: Using the parameters is only needed if you want
+    // to support moving the FAB around the screen.
+    // NOTE: This immediately hides the FAB. An animation can
+    // be used instead - see the sample app.
+    //setVisibility(View.VISIBLE);
+    super.show();
+  }
+
+  /**
+   * Hides the FAB.
+   */
+  @Override
+  public void hide() {
+    // NOTE: This immediately hides the FAB. An animation can
+    // be used instead - see the sample app.
+    //setVisibility(View.GONE);
+    super.hide();
+  }
+}
